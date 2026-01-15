af90d89e305116b292f83329d7dbe03a87a7fc4f
Sarah Ball <snball6@gmail.com>
2026-01-14 20:22:47 -0500

    categories computed and out saves to file

--- python-timesheet-parsing/timesheet_parser.py
+++ python-timesheet-parsing/timesheet_parser.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from pathlib import Path
 
-def parse_first_sheet_from_xlsx(folder_path):
+def parse_timesheets(folder_path, output_file_name):
     """
-    TODO DESCRIPTION
+    Iterate through a folder of Menlo timesheets to extract hours worked and determine holidays, workdays, and pto
 
     Args:
-        folder_path (str): The path to the timesheet files.
+        folder_path (Path): The path folder containing only timesheets
+        output_file_name (str): Name for output file
     """
     try:      
         timesheet_data = pd.DataFrame(columns=["date","hours","week_ending","week_pto","category"])
@@ -20,23 +21,28 @@ def parse_first_sheet_from_xlsx(folder_path):
                 print(f"Reading'{file_path}'")
                 
                 week_ending = df.iloc[1,2]
-                mon_thru_fri_dates = df.iloc[3,6:11].tolist()
+
+                # template changes have dates in one of two rows
+                if df.iloc[3,4] == "Sat":
+                    mon_thru_fri_dates = df.iloc[4,4:11].tolist()
+                else:
+                    mon_thru_fri_dates = df.iloc[3,4:11].tolist()
+
                 mon_thru_fri_totals = []
                 week_pto = ""
                 for index, row in df.iterrows():
                     # template changes between 2023 and 2024 moved the daily totals text over a column due to a merged cell  
                     if(row.iloc[2]=="Daily totals:" or row.iloc[3]=="Daily totals:"):
-                        mon_thru_fri_totals = row.iloc[6:11]
+                        mon_thru_fri_totals = row.iloc[4:11].tolist()
 
                     if(row.iloc[6]=="PTO:"):
                         week_pto = row.iloc[10]
 
-                    # if("holiday" in row.iloc[2].lower()):
-                    #     print('found')
-                    #     pass
-                        #check for column with 8 on this row, then get the corresponding date
-
-
+                    if("holiday" in str(row.iloc[2]).lower()):
+                        mon_thru_fri_holiday = row.iloc[4:11].tolist()
+                        for index, cell_value in enumerate(mon_thru_fri_holiday):
+                            if cell_value > 0:
+                                holidays.add(mon_thru_fri_dates[index])
 
                 if len(mon_thru_fri_totals)<5:
                     flagged_sheets.add(file_path.name)
@@ -50,14 +56,38 @@ def parse_first_sheet_from_xlsx(folder_path):
                     non_empty_dfs = [df for df in [weekly_data, timesheet_data] if not df.empty]
                     timesheet_data = pd.concat(non_empty_dfs, ignore_index=True)
 
-        print(timesheet_data)
         print("unable to parse", flagged_sheets)
+
+        result = set_category(timesheet_data, holidays)
+        result.to_csv(output_file_name, index=False)
+
 
     except ValueError as e:
         print(f"Error: Could not read the Excel file. Details: {e}")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
+def set_category(timesheet_data, holidays):
+    timesheet_data['category'] = timesheet_data.apply(lambda row: category_by_time(row['hours'], row['date']), axis=1)
+    timesheet_data.loc[timesheet_data['date'].isin(holidays), 'category'] = 'holiday'
+    return timesheet_data
+
+
+def category_by_time(hours, date):
+    if hours >= 6:
+        return "workday"
+    elif 6 > hours >= 1:
+        return "partial"
+    elif 1 > hours:
+        if date.weekday() in [4, 5, 6]:
+            return "weekend"
+        else:
+            return "pto"
+
+
+
 if __name__ == "__main__":
-    folder_path = Path("./sample_timesheets")
-    parse_first_sheet_from_xlsx(folder_path)
+    folder_path = Path("./actual_timesheets")
+    output_file_name = "output.csv"
+    parse_timesheets(folder_path, output_file_name)
+    print("Done!")
