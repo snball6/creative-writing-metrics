4b14b6f8919daf77b7f1b40e0119425db3744c6e
Sarah Ball <snball6@gmail.com>
2026-01-15 19:13:42 -0500

    Additional validation checks and better output warnings. Visualization of hours. Change tracking of manual adjustments to source data.

--- python-timesheet-parsing/timesheet_parser.py
+++ python-timesheet-parsing/timesheet_parser.py
@@ -1,22 +1,18 @@
 import pandas as pd
+import calplot
+import matplotlib.pyplot as plt
 from pathlib import Path
 
-def parse_timesheets(folder_path, output_file_name):
-    """
-    Iterate through a folder of Menlo timesheets to extract hours worked and determine holidays, workdays, and pto
-
-    Args:
-        folder_path (Path): The path folder containing only timesheets
-        output_file_name (str): Name for output file
-    """
+def parse_timesheets(folder_path):
     try:      
-        timesheet_data = pd.DataFrame(columns=["date","hours","week_ending","week_pto","category"])
+        timesheet_data = pd.DataFrame(columns=["date","hours","week_ending","week_pto"])
         flagged_sheets = set()
         holidays = set()
+        issues_list = []
 
         for file_path in folder_path.iterdir():
             if file_path.is_file():
-                # timesheet data always on the first sheet
+                # timesheet data is always on the first sheet
                 df = pd.read_excel(file_path, sheet_name=0)
                 print(f"Reading'{file_path}'")
                 
@@ -31,7 +27,8 @@ def parse_timesheets(folder_path, output_file_name):
                 mon_thru_fri_totals = []
                 week_pto = ""
                 for index, row in df.iterrows():
-                    # template changes between 2023 and 2024 moved the daily totals text over a column due to a merged cell  
+
+                    # template changes moved the daily totals text over a column due to a merged cell  
                     if(row.iloc[2]=="Daily totals:" or row.iloc[3]=="Daily totals:"):
                         mon_thru_fri_totals = row.iloc[4:11].tolist()
 
@@ -44,23 +41,40 @@ def parse_timesheets(folder_path, output_file_name):
                             if cell_value > 0:
                                 holidays.add(mon_thru_fri_dates[index])
 
+
                 if len(mon_thru_fri_totals)<5:
+                    issues_list.append(("File: " + file_path.name, "failed to find totals"))
                     flagged_sheets.add(file_path.name)
-                    print("Failed to find totals")
-                elif week_pto == "":
+                if week_pto == "":
+                    issues_list.append(("File: " + file_path.name, "failed to find pto"))
                     flagged_sheets.add(file_path.name)
-                    print("Failed to find pto")
-                else:
+
+                if file_path.name not in flagged_sheets:
                     weekly_data = pd.DataFrame({"date": mon_thru_fri_dates, "hours": mon_thru_fri_totals, 
                                                 "week_ending": week_ending, "week_pto": week_pto})
                     non_empty_dfs = [df for df in [weekly_data, timesheet_data] if not df.empty]
                     timesheet_data = pd.concat(non_empty_dfs, ignore_index=True)
 
-        print("unable to parse", flagged_sheets)
+        # Check for gaps in the date range
+        full_range = pd.date_range(start=timesheet_data["date"].min(), end=timesheet_data["date"].max(), freq='D')
+        actual_dates_set = set(timesheet_data['date'].dt.date)
+        expected_dates_set = set(full_range.date)
+        missing_dates = expected_dates_set - actual_dates_set
 
-        result = set_category(timesheet_data, holidays)
-        result.to_csv(output_file_name, index=False)
+        dates_list = list(missing_dates)
+        dates_list.sort()
+        for date in dates_list:
+            issues_list.append(("Date: " + date.strftime("%y%m%d"), "Missing"))
 
+        print("-------------------\n\n\n")
+        print("Unable to parse:")
+        for flagged in flagged_sheets:
+            print(flagged)
+        print("Issues found: ")
+        for issue in issues_list:
+            print(issue)
+        
+        return (timesheet_data, holidays)
 
     except ValueError as e:
         print(f"Error: Could not read the Excel file. Details: {e}")
@@ -69,7 +83,7 @@ def parse_timesheets(folder_path, output_file_name):
 
 def set_category(timesheet_data, holidays):
     timesheet_data['category'] = timesheet_data.apply(lambda row: category_by_time(row['hours'], row['date']), axis=1)
-    timesheet_data.loc[timesheet_data['date'].isin(holidays), 'category'] = 'holiday'
+    timesheet_data.loc[timesheet_data["date"].isin(holidays), 'category'] = 'holiday'
     return timesheet_data
 
 
@@ -84,10 +98,18 @@ def category_by_time(hours, date):
         else:
             return "pto"
 
+def generate_heatmap(timesheet_data):
+    df = timesheet_data.set_index('date')
 
+    calplot.calplot(df['hours'], edgecolor=None, cmap='YlGn')
+    plt.show()
 
 if __name__ == "__main__":
     folder_path = Path("./actual_timesheets")
     output_file_name = "output.csv"
-    parse_timesheets(folder_path, output_file_name)
-    print("Done!")
+
+    timesheet_data, holidays = parse_timesheets(folder_path)
+    generate_heatmap(timesheet_data)
+    result = set_category(timesheet_data, holidays)
+    result.to_csv(output_file_name, index=False)
+    print("Results saved to: " + output_file_name)
